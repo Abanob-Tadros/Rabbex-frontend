dde3af2541a64524918eb78e668fe21cc616de1a
bebo <abnobpop67@gmail.com>
2026-08-04 15:55:18 +0300

    logo

---
+++ components/layout/Navbar/HorizontalNavbar.tsx
@@ -0,0 +1,20 @@
+//Next
+import Link from "next/link";
+//Logo
+import Logo from "@/components/Logo";
+//types
+// import { INavbarData } from "@/src/types/types";
+
+//-----------------------------
+type Props = {
+  navbarData: INavbarData;
+  locale: string;
+};
+
+//----------------------------
+export default function HorizontalNavbar({ navbarData, locale }: Props) {
+  return (
+   <>
+   </>
+  );
+}
